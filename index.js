c7d70a1e71427de5f1bcf75caeb36997e8ffbd50
Phillip Temple <phillipcorteztemple@gmail.com>
2018-11-27 17:51:31 -0600

    fining JS

--- index.js
+++ index.js
@@ -1,31 +1,15 @@
-import Footer from './src/Footer';
 import Content from './src/Content';
+import Footer from './src/Footer';
+import greet from './src/Greeting';
 import Header from './src/Header';
 import Navigation from './src/Navigation';
 
 
 document.querySelector('#root').innerHTML = `
+    ${Navigation}
+    ${Header}
     ${Content}
     ${Footer}
-    ${Header}
-    ${Navigation}
-    `;
+`;
 
-
-// var title = document.querySelector('h1');
-
-
-// var greet = function greet(){
-//  if(!userName){
-//    userName = prompt('What is your name Please');
-//
-// greet();
-// }
-// else{
-//   title.textContent += `, + ${userName}`;
-// }
-// };
-
-// greet();
-
-// var userName = prompt('What is your name') || 'Anonymous User';
+greet();
